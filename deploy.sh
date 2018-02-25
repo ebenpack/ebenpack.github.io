64f11eebb8e75d21aaa4d81c6f0d2572c3d2cfc4
ebenpack <ebenpackwood@gmail.com>
2018-02-24 22:02:23 -0500

    Add pushprompt

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,7 @@ git checkout develop
 npm install
 
 # Build new files
+stack build
 stack exec site clean
 stack exec site build
 
@@ -30,6 +31,13 @@ cp -a _site/. .
 # Commit
 git add -A
 git commit -m "publish."
+
+read -p $'Enter Y to push\x0a' -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
 
 # Push
 git push origin publish:master
