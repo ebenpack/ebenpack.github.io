f080eec3ca75975a6360fce09be8b348aff24e22
ebenpack <ebenpackwood@gmail.com>
2018-04-08 19:23:01 -0400

    Format

--- site.hs
+++ site.hs
@@ -15,109 +15,121 @@ import Text.Pandoc.Options
 
 --------------------------------------------------------------------------------
 main :: IO ()
-main =
-  hakyllWith config $ do
+main = hakyllWith config $ do
     match "assets/**" $ do
-      route assetRoute
-      compile copyFileCompiler
+        route assetRoute
+        compile copyFileCompiler
     match "posts/*" $ do
-      route $ composeRoutes slugRoute $ setExtension "html"
-      compile $
-        customPandocCompiler >>= saveSnapshot "content" >>=
-        loadAndApplyTemplate "templates/post.tmpl" postCtx >>=
-        loadAndApplyTemplate "templates/default.tmpl" postCtx >>=
-        relativizeUrls
+        route $ composeRoutes slugRoute $ setExtension "html"
+        compile
+            $   customPandocCompiler
+            >>= saveSnapshot "content"
+            >>= loadAndApplyTemplate "templates/post.tmpl"    postCtx
+            >>= loadAndApplyTemplate "templates/default.tmpl" postCtx
+            >>= relativizeUrls
     match "pages/projects/*" $ do
-      route $ composeRoutes slugRoute $ setExtension "html"
-      compile $
-        customPandocCompiler >>=
-        loadAndApplyTemplate "templates/project.tmpl" postCtx >>=
-        loadAndApplyTemplate "templates/default.tmpl" postCtx >>=
-        relativizeUrls
+        route $ composeRoutes slugRoute $ setExtension "html"
+        compile
+            $   customPandocCompiler
+            >>= loadAndApplyTemplate "templates/project.tmpl" postCtx
+            >>= loadAndApplyTemplate "templates/default.tmpl" postCtx
+            >>= relativizeUrls
     match "pages/*" $ do
-      route $ composeRoutes slugRoute $ setExtension "html"
-      compile $
-        customPandocCompiler >>=
-        loadAndApplyTemplate "templates/page.tmpl" postCtx >>=
-        loadAndApplyTemplate "templates/default.tmpl" postCtx >>=
-        relativizeUrls
+        route $ composeRoutes slugRoute $ setExtension "html"
+        compile
+            $   customPandocCompiler
+            >>= loadAndApplyTemplate "templates/page.tmpl"    postCtx
+            >>= loadAndApplyTemplate "templates/default.tmpl" postCtx
+            >>= relativizeUrls
     create ["pages/projects.html"] $ do
-      route idRoute
-      compile $ do
-        projects <- loadAll "pages/projects/*" >>= sortChronologicalItems
-        let projectCtx =
-              listField "projects" defaultContext (return projects) <>
-              constField "title" "Projects" <>
-              defaultContext
-        makeItem "" >>=
-          loadAndApplyTemplate "templates/projects.tmpl" projectCtx >>=
-          loadAndApplyTemplate "templates/default.tmpl" projectCtx >>=
-          relativizeUrls
+        route idRoute
+        compile $ do
+            projects <- loadAll "pages/projects/*" >>= sortChronologicalItems
+            let projectCtx =
+                    listField "projects" defaultContext (return projects)
+                        <> constField "title" "Projects"
+                        <> defaultContext
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/projects.tmpl" projectCtx
+                >>= loadAndApplyTemplate "templates/default.tmpl"  projectCtx
+                >>= relativizeUrls
     create ["archives.html"] $ do
-      route idRoute
-      compile $ do
-        posts <- loadAll "posts/*" >>= groupChronologicalItems
-        let archiveCtx =
-              listField
-                "dates"
-                (field
-                   "date"
-                   (return .
-                    formatTime defaultTimeLocale "%B %0Y" . fst . itemBody) <>
-                 listFieldWith "posts" teaserCtx (return . snd . itemBody))
-                (traverse (\(d, is) -> makeItem (d, is)) posts) <>
-              constField "title" "Archives" <>
-              defaultContext
-        makeItem "" >>=
-          loadAndApplyTemplate "templates/archives.tmpl" archiveCtx >>=
-          loadAndApplyTemplate "templates/default.tmpl" archiveCtx >>=
-          relativizeUrls
+        route idRoute
+        compile $ do
+            posts <- loadAll "posts/*" >>= groupChronologicalItems
+            let
+                archiveCtx =
+                    listField
+                            "dates"
+                            (  field
+                                    "date"
+                                    ( return
+                                    . formatTime defaultTimeLocale "%B %0Y"
+                                    . fst
+                                    . itemBody
+                                    )
+                            <> listFieldWith
+                                   "posts"
+                                   teaserCtx
+                                   (return . snd . itemBody)
+                            )
+                            (traverse (\(d, is) -> makeItem (d, is)) posts)
+                        <> constField "title" "Archives"
+                        <> defaultContext
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/archives.tmpl" archiveCtx
+                >>= loadAndApplyTemplate "templates/default.tmpl"  archiveCtx
+                >>= relativizeUrls
     page <- paginate "posts/*"
     paginateRules page $ \pageNum pattern' -> do
-      route idRoute
-      compile $ do
-        posts <- recentFirst =<< loadAllSnapshots pattern' "content"
-        let paginateCtx = paginateContext page pageNum
-            indexCtx =
-              listField "posts" teaserCtx (return posts) <>
-              constField "title" "Home" <>
-              paginateContextPlus page pageNum <>
-              defaultContext
-        makeItem "" >>= loadAndApplyTemplate "templates/index.tmpl" indexCtx >>=
-          loadAndApplyTemplate "templates/default.tmpl" indexCtx >>=
-          relativizeUrls
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAllSnapshots pattern' "content"
+            let paginateCtx = paginateContext page pageNum
+                indexCtx =
+                    listField "posts" teaserCtx (return posts)
+                        <> constField          "title" "Home"
+                        <> paginateContextPlus page    pageNum
+                        <> defaultContext
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/index.tmpl"   indexCtx
+                >>= loadAndApplyTemplate "templates/default.tmpl" indexCtx
+                >>= relativizeUrls
     match "templates/404.tmpl" $ do
-      route idRoute
-      compile $
-        getResourceBody >>= applyAsTemplate postCtx >>=
-        loadAndApplyTemplate "templates/default.tmpl" postCtx >>=
-        relativizeUrls
+        route idRoute
+        compile
+            $   getResourceBody
+            >>= applyAsTemplate postCtx
+            >>= loadAndApplyTemplate "templates/default.tmpl" postCtx
+            >>= relativizeUrls
     match "templates/*" $ compile templateBodyCompiler
 
 --------------------------------------------------------------------------------
 -- Config
 config :: Configuration
-config = defaultConfiguration {deployCommand = "./deploy.sh"}
+config = defaultConfiguration { deployCommand = "./deploy.sh" }
 
 -- Compiler
 customPandocCompiler :: Compiler (Item String)
 customPandocCompiler =
-  let defaultWriterExtensions = writerExtensions defaultHakyllWriterOptions
-      newWriterExtensions =
-        enableExtension Ext_raw_attribute defaultWriterExtensions
-      writerOptions =
-        defaultHakyllWriterOptions {writerExtensions = newWriterExtensions}
-  in pandocCompilerWith defaultHakyllReaderOptions writerOptions
+    let
+        defaultWriterExtensions = writerExtensions defaultHakyllWriterOptions
+        newWriterExtensions =
+            enableExtension Ext_raw_attribute defaultWriterExtensions
+        writerOptions = defaultHakyllWriterOptions
+            { writerExtensions = newWriterExtensions
+            }
+    in
+        pandocCompilerWith defaultHakyllReaderOptions writerOptions
 
 -- Routing
 slugRoute :: Routes
 slugRoute = metadataRoute makeRoute
   where
     makeRoute :: Metadata -> Routes
-    makeRoute md =
-      case lookupString "slug" md of
+    makeRoute md = case lookupString "slug" md of
         Just slug -> customRoute $ slugSub slug . toFilePath
-        Nothing -> idRoute
+        Nothing   -> idRoute
     slugSub :: String -> FilePath -> FilePath
     slugSub slug path = (joinPath . init . splitPath) path </> slug
 
@@ -135,38 +147,34 @@ teaserCtx :: Context String
 teaserCtx = teaserField "teaser" "content" <> postCtx
 
 -- Archives
-groupChronologicalItems :: [Item a] -> Compiler [(UTCTime, [Item a])]
 groupChronologicalItems items = do
-  withTime <-
-    forM items $ \item -> do
-      utc <- getItemUTC defaultTimeLocale $ itemIdentifier item
-      return (utc, [item])
-  return $
-    fmap merge $ groupBy compareTime $ sortBy (flip (comparing fst)) withTime
+    withTime <- forM items $ \item -> do
+        utc <- getItemUTC defaultTimeLocale $ itemIdentifier item
+        return (utc, [item])
+    return $ fmap merge $ groupBy compareTime $ sortBy
+        (flip (comparing fst))
+        withTime
   where
     merge :: [(UTCTime, [Item a])] -> (UTCTime, [Item a])
     merge groups =
-      let conv (date, acc) (_, toAcc) = (date, toAcc ++ acc)
-      in foldr conv (head groups) (tail groups)
+        let conv (date, acc) (_, toAcc) = (date, toAcc ++ acc)
+        in  foldr conv (head groups) (tail groups)
     compareTime :: (UTCTime, a) -> (UTCTime, b) -> Bool
     compareTime (t1, _) (t2, _) =
-      let (year1, month1, _) = toGregorian $ utctDay t1
-          (year2, month2, _) = toGregorian $ utctDay t2
-      in year1 == year2 && month1 == month2
+        let (year1, month1, _) = toGregorian $ utctDay t1
+            (year2, month2, _) = toGregorian $ utctDay t2
+        in  year1 == year2 && month1 == month2
 
 sortChronologicalItems :: [Item a] -> Compiler [Item a]
 sortChronologicalItems items = do
-  withTime <-
-    forM items $ \item -> do
-      utc <- getItemUTC defaultTimeLocale $ itemIdentifier item
-      return (utc, item)
-  return $ map snd $ sortBy (flip (comparing fst)) withTime
+    withTime <- forM items $ \item -> do
+        utc <- getItemUTC defaultTimeLocale $ itemIdentifier item
+        return (utc, item)
+    return $ map snd $ sortBy (flip (comparing fst)) withTime
 
 -- Pagination
 makeId :: PageNumber -> Identifier
-makeId pageNum =
-  fromFilePath $
-  if pageNum == 1
+makeId pageNum = fromFilePath $ if pageNum == 1
     then "index.html"
     else "index" ++ show pageNum ++ ".html"
 
@@ -178,25 +186,24 @@ paginate pattern' = buildPaginateWith grouper pattern' makeId
 
 paginateContextPlus :: Paginate -> PageNumber -> Context a
 paginateContextPlus page currentPage =
-  paginateContext page currentPage <>
-  mconcat
-    [ listField "pagesBefore" linkCtx $ wrapPages pagesBefore
-    , listField "pagesAfter" linkCtx $ wrapPages pagesAfter
-    ]
+    paginateContext page currentPage <> mconcat
+        [ listField "pagesBefore" linkCtx $ wrapPages pagesBefore
+        , listField "pagesAfter"  linkCtx $ wrapPages pagesAfter
+        ]
   where
     linkCtx :: Context (String, String)
-    linkCtx =
-      field "pageNum" (return . fst . itemBody) <>
-      field "pageUrl" (return . snd . itemBody)
+    linkCtx = field "pageNum" (return . fst . itemBody)
+        <> field "pageUrl" (return . snd . itemBody)
     pages :: [(Int, Identifier)]
-    pages = [pageInfo n | n <- [1 .. lastPage], n /= currentPage]
+    pages = [ pageInfo n | n <- [1 .. lastPage], n /= currentPage ]
     lastPage :: Int
     lastPage = M.size . paginateMap $ page
     pageInfo :: Int -> (Int, Identifier)
     pageInfo n = (n, paginateMakeId page n)
     (pagesBefore, pagesAfter) =
-      span ((< currentPage) . fst) pages :: ( [(PageNumber, Identifier)]
-                                            , [(PageNumber, Identifier)])
+        span ((< currentPage) . fst) pages :: ( [(PageNumber, Identifier)]
+            , [(PageNumber, Identifier)]
+            )
 
 wrapPages :: [(PageNumber, Identifier)] -> Compiler [Item (String, String)]
 wrapPages = mapM makeInfoItem
@@ -205,7 +212,6 @@ makeInfoItem :: (PageNumber, Identifier) -> Compiler (Item (String, String))
 makeInfoItem (n, i) = getRoute i >>= makeInfo
   where
     makeInfo :: Maybe FilePath -> Compiler (Item (String, String))
-    makeInfo filepath =
-      case filepath of
-        Just p -> makeItem (show n, toUrl p)
+    makeInfo filepath = case filepath of
+        Just p  -> makeItem (show n, toUrl p)
         Nothing -> fail $ "No URL for page: " ++ show n
